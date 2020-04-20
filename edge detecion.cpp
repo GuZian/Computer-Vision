d07656bd384ba7520ffe9dcdd89b06227196bfb6
Zian Gu <39391211+GuZian@users.noreply.github.com>
2020-04-20 22:58:58 +0800

    Update edge detecion.cpp

--- edge detecion.cpp
+++ edge detecion.cpp
@@ -28,8 +28,8 @@ int main(int argc, char** argv) {
 	Mat imageS;//缩小后的图像
 	Mat imageConverted;
 	Mat imageS_Blur;//通过中值滤波后的图像
-	Mat imageSobelx, imageSobely;//x,y方向使用Sobel算子卷积所得的图像
-	Mat imageSobelxAbs, imageSobelyAbs;//x,y方向使用Sobel算子卷积所得的图像并取绝对值
+	Mat imageX, imageY;//x,y方向卷积所得的图像
+	Mat imageXAbs, imageYAbs;//x,y方向卷积所得的图像并取绝对值
 	Mat imageSobel;//最终通过Sobel算子检测的边缘图像
 
 	//Mat Gx = (Mat_<int>(3, 3) <<// Scharr算子横（x）向处理的卷积核
@@ -56,16 +56,17 @@ int main(int argc, char** argv) {
 
 	resize(image, imageS, imageS.size(), 0.16, 0.2, 1);//改变图像大小，以完整显示
 
-	//medianBlur(imageS, imageS_Blur, 5);//对图像进行中值滤波，否则噪声太大效果不好(也可以用高斯滤波等等尝试一下哪个效果更好，在这里暂时使用中值滤波)。经实验卷积核越大，效果越好，取值范围1-5。
+	//medianBlur(imageS, imageS_Blur, 5);//对图像进行中值滤波，否则噪声太大效果不好(也可以用高斯滤波等等尝试一下哪个效果更好，在这里暂时使用中值滤波)。经实验卷积核越大，效果越好，取值为正奇数。
 	GaussianBlur(imageS,imageS_Blur,Size(5,5),0);//对图像进行高斯滤波。原理同上。
 
-	filter2D(imageS_Blur,imageSobelx,CV_64F,Gx);//x方向卷积
-	convertScaleAbs(imageSobelx, imageSobelxAbs);//需要取绝对值，否则卷积所得负值在显示的时候默认为0，将损失边界
-	filter2D(imageS_Blur, imageSobely, CV_64F, Gy);//y方向卷积
-	convertScaleAbs(imageSobely, imageSobelyAbs);//同理
-	addWeighted(imageSobelxAbs, 0.5, imageSobelyAbs, 0.5, 0, imageSobel);//按权重将两个方向卷积所得图像融合
+	filter2D(imageS_Blur,imageX,CV_64F,Gx);//x方向卷积
+	convertScaleAbs(imageX, imageXAbs);//需要取绝对值，否则卷积所得负值在显示的时候默认为0，将损失边界
+	filter2D(imageS_Blur, imageY, CV_64F, Gy);//y方向卷积
+	convertScaleAbs(imageY, imageYAbs);//同理
+	addWeighted(imageXAbs, 0.5, imageYAbs, 0.5, 0, imageSobel);//按权重将两个方向卷积所得图像融合
 	//cout << imageS.size();
 	imgShow("Jani", imageSobel);//输出最终结果
+	
 	return 0;
 
 }
